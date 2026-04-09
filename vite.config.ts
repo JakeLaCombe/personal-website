fd5917f0c393cebda1081193c24c6207f121da56
Jake LaCombe <jake@crema.us>
2026-04-09 15:01:36 -0500

    Fix Build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === "serve" ? "/" : "/goalie-rush/",
+  base: command === "serve" ? "/" : "/personal-website/",
 }));
 
 // vi
+g
